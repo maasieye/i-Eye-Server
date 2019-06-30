13c587d0ffdb1b74dc532b42bedbfa42465e6ce8
pci2676 <pci2676@gmail.com>
2019-06-30 20:48:24 +0900

    프로젝트생성

---
+++ src/main/java/kr/seoulmaas/ieye/IeyeApplication.java
@@ -0,0 +1,13 @@
+package kr.seoulmaas.ieye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeyeApplication.class, args);
+	}
+
+}
