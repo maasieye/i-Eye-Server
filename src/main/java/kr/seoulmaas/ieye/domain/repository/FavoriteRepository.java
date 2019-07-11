5e01c313f80303a2b915aae37b6e77abde139b0c
pci2676 <pci2676@gmail.com>
2019-07-11 22:34:45 +0900

    ignore 재설정.

--- src/main/java/kr/seoulmaas/ieye/domain/repository/FavoriteRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.seoulmaas.ieye.domain.repository;
-
-import kr.seoulmaas.ieye.domain.entity.Favorite;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-}
